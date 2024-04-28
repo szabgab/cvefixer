6ac3b73fc2b8c439182e2bfaad2c89a298c4519a
12ball <pub@janie.page>
2024-04-28 08:24:26 -0500

    Add more logging to hooks

--- lib/taskgroups/hooks.rb
+++ lib/taskgroups/hooks.rb
@@ -7,11 +7,14 @@ class Hooks
     if config["hooks"].is_a? Array
       config["hooks"].each do |hook|
         if hook.is_a? String
+          l.info "running hook #{hook}"
           system hook
         else
           l.error "bad hook given in config: #{hook}"
         end
       end
+    else
+      l.info "not running hook scripts; no config file found"
     end
   end
 end
