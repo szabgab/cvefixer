b5cf1cd26538e957a96d718daeffec41f05fc53e
Jane Davis <61095764+janie314@users.noreply.github.com>
2024-05-09 21:03:41 -0500

    v0.9.0 (#36)
    * Display help text when no arguments provided
    * Add `which` dependency
    * Update dependencies
    
    ---------
    
    Co-authored-by: 12ball <pub@janie.page>

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,6 +2,7 @@ use log::debug;
 
 use crate::errors::Error;
 use std::process::{Command, Stdio};
+use which::which;
 
 pub fn exec(cmd: &mut Command) -> Result<(), Error> {
     debug!("executing {:?}", &cmd);
@@ -13,23 +14,8 @@ pub fn exec(cmd: &mut Command) -> Result<(), Error> {
     }
 }
 
-pub fn exec_silent(cmd: &mut Command) -> Result<(), Error> {
-    debug!("executing {:?}", &cmd);
-    let res = cmd
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()?;
-    if res.success() {
-        Ok(())
-    } else {
-        Err(Error::NonZeroExitStatus)
-    }
-}
-
 pub fn cmd_exists(cmd: &str) -> bool {
-    debug!("checking if {:?} exists", &cmd);
-    match exec_silent(Command::new("command").arg(cmd)) {
+    match which(cmd) {
         Ok(_) => true,
         Err(_) => false,
     }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod task;
 
 /// fix all vulnerabilities that are fixed
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
 struct Args {
     #[command(subcommand)]
     command: Option<Commands>,
